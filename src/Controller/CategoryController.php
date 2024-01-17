8138df65e17d79a6695718f9fff9acbdca2a4cfa
Jensone <hello@jensone.com>
2024-01-17 11:22:11 +0100

    Suppression des "use" inutiles

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Category;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
