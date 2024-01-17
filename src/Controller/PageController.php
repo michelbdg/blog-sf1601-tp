fb1314141a51e61495e875e61438c58d9e74d64b
Jensone <hello@jensone.com>
2024-01-17 11:21:05 +0100

    Mise en place de la pagination

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -5,33 +5,37 @@ namespace App\Controller;
 use App\Entity\Category;
 use App\Repository\PostRepository;
 use App\Repository\CategoryRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\Pagination\PaginationInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PageController extends AbstractController
 {
-    #[Route('/', name: 'home')]
+    #[Route('/', name: 'home', methods: ['GET'])]
     public function index(
-        // Inject the post repository to find all posts
+        Request $request,
         PostRepository $postRepository,
-        // Inject the category repository to find all categories
-        CategoryRepository $categoryRepository
+        CategoryRepository $categoryRepository,
+        PaginatorInterface $paginator
     ): Response {
-        // Return the view
+        $posts = $paginator->paginate(
+            $postRepository->findAll(), // Request
+            $request->query->getInt('page', 1), // Page number
+            10 // Limit per page
+        );
         return $this->render('page/home.html.twig', [
-            // Pass the page title to the view
-            'posts' => $postRepository->findAll(),
-            // Pass all categories to the view
+            'posts' => $posts,
             'categories' => $categoryRepository->findAll()
         ]);
     }
 
-    // Route for displaying a single category wi
-    #[Route('/{category}', name: 'category')]
+    // Route for displaying a single category (NE PAS REECRIRE TOUS LES COMMENTAIRES)
+    #[Route('/{category}', name: 'category', methods: ['GET'])]
     public function category(
-        // Inject the request object to get the category name
+        // Inject the request object to get the category name (NE PAS REECRIRE TOUS LES COMMENTAIRES)
         Request $request,
         // Inject the category repository to find the category
         CategoryRepository $categoryRepository,
