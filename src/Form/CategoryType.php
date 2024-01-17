257c138eb771fd37bc2e49468304dea2d5ef4a5e
Jensone <hello@jensone.com>
2024-01-17 15:44:22 +0100

    Ajout des Type au formulaire

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -4,11 +4,11 @@ namespace App\Form;
 
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CategoryType extends AbstractType
 {
@@ -18,6 +18,7 @@ class CategoryType extends AbstractType
             ->add('name', TextType::class)
             ->add('image', FileType::class, [
                 'mapped' => false,
+                'required' => false
             ])
             ->add('Enregistrer', SubmitType::class)
         ;
